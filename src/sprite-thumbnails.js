bcdb8fcc8572e2cafb4df0d4378f13969e46e80f
Christian Ebert <blacktrash@gmx.net>
2020-10-04 01:09:24 +0100

    refactor: determine mouse position based on getPointerPosition

--- src/sprite-thumbnails.js
+++ src/sprite-thumbnails.js
@@ -19,6 +19,7 @@ export default function spriteThumbs(player, options) {
   const controls = player.controlBar;
   const progress = controls.progressControl;
   const seekBar = progress.seekBar;
+  const seekBarEl = seekBar.el();
   const mouseTimeDisplay = seekBar.mouseTimeDisplay;
 
   if (url && height && width && mouseTimeDisplay) {
@@ -39,46 +40,43 @@ export default function spriteThumbs(player, options) {
       });
     };
 
-    const hijackMouseTooltip = () => {
+    const hijackMouseTooltip = (evt) => {
       const imgWidth = img.naturalWidth;
       const imgHeight = img.naturalHeight;
 
       if (player.controls() && imgWidth && imgHeight) {
-        let hoverPosition = parseFloat(mouseTimeDisplay.el().style.left);
+        let position = dom.getPointerPosition(seekBarEl, evt).x * player.duration();
 
-        hoverPosition = player.duration() * (hoverPosition / seekBar.currentWidth());
-        if (!isNaN(hoverPosition)) {
-          hoverPosition = hoverPosition / options.interval;
+        position = position / options.interval;
 
-          const playerWidth = player.currentWidth();
-          const scaleFactor = responsive && playerWidth < responsive ?
-            playerWidth / responsive : 1;
-          const columns = imgWidth / width;
-          const scaledWidth = width * scaleFactor;
-          const scaledHeight = height * scaleFactor;
-          const cleft = Math.floor(hoverPosition % columns) * -scaledWidth;
-          const ctop = Math.floor(hoverPosition / columns) * -scaledHeight;
-          const bgSize = (imgWidth * scaleFactor) + 'px ' +
-                         (imgHeight * scaleFactor) + 'px';
-          const controlsTop = dom.findPosition(controls.el()).top;
-          const seekBarTop = dom.findPosition(seekBar.el()).top;
-          // top of seekBar is 0 position
-          const topOffset = -scaledHeight - Math.max(0, seekBarTop - controlsTop);
+        const playerWidth = player.currentWidth();
+        const scaleFactor = responsive && playerWidth < responsive ?
+          playerWidth / responsive : 1;
+        const columns = imgWidth / width;
+        const scaledWidth = width * scaleFactor;
+        const scaledHeight = height * scaleFactor;
+        const cleft = Math.floor(position % columns) * -scaledWidth;
+        const ctop = Math.floor(position / columns) * -scaledHeight;
+        const bgSize = (imgWidth * scaleFactor) + 'px ' +
+                       (imgHeight * scaleFactor) + 'px';
+        const controlsTop = dom.findPosition(controls.el()).top;
+        const seekBarTop = dom.findPosition(seekBarEl).top;
+        // top of seekBar is 0 position
+        const topOffset = -scaledHeight - Math.max(0, seekBarTop - controlsTop);
 
-          tooltipStyle({
-            'width': scaledWidth + 'px',
-            'height': scaledHeight + 'px',
-            'background-image': 'url(' + url + ')',
-            'background-repeat': 'no-repeat',
-            'background-position': cleft + 'px ' + ctop + 'px',
-            'background-size': bgSize,
-            'top': topOffset + 'px',
-            'color': '#fff',
-            'text-shadow': '1px 1px #000',
-            'border': '1px solid #000',
-            'margin': '0 1px'
-          });
-        }
+        tooltipStyle({
+          'width': scaledWidth + 'px',
+          'height': scaledHeight + 'px',
+          'background-image': 'url(' + url + ')',
+          'background-repeat': 'no-repeat',
+          'background-position': cleft + 'px ' + ctop + 'px',
+          'background-size': bgSize,
+          'top': topOffset + 'px',
+          'color': '#fff',
+          'text-shadow': '1px 1px #000',
+          'border': '1px solid #000',
+          'margin': '0 1px'
+        });
       }
     };
 
