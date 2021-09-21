b85f57d1c264681f2a5f88690556b392aebd46e1
Christian Ebert <bcc@phloxic.productions>
2021-09-21 18:46:17 +0200

    refactor: make player level (only) configuration obvious
    Clarify advantage of this setup: When there is no playlist the player
    can preload the single sprite.

--- src/sprite-thumbnails.js
+++ src/sprite-thumbnails.js
@@ -1,4 +1,5 @@
 import videojs from 'video.js';
+import window from 'global/window';
 
 /**
  * Set up sprite thumbnails for a player.
@@ -9,77 +10,34 @@ import videojs from 'video.js';
  * @param {Object} options
  *        Configuration options.
  */
-export default function spriteThumbs(player, options) {
+const spriteThumbs = (player, options) => {
   const url = options.url;
   const height = options.height;
   const width = options.width;
-  const responsive = options.responsive;
+
+  const sprite = new window.Image();
+  const log = player.spriteThumbnails().log;
 
   const dom = videojs.dom || videojs;
   const controls = player.controlBar;
   const progress = controls.progressControl;
   const seekBar = progress.seekBar;
-  const seekBarEl = seekBar.el();
   const mouseTimeDisplay = seekBar.mouseTimeDisplay;
 
-  if (url && height && width && mouseTimeDisplay) {
-    const img = dom.createEl('img', {
-      src: url
-    });
+  const tooltipStyle = (obj) => {
+    Object.keys(obj).forEach((key) => {
+      const val = obj[key];
+      const ttstyle = mouseTimeDisplay.timeTooltip.el().style;
 
-    const tooltipStyle = (obj) => {
-      Object.keys(obj).forEach((key) => {
-        const val = obj[key];
-        const ttstyle = mouseTimeDisplay.timeTooltip.el().style;
-
-        if (val !== '') {
-          ttstyle.setProperty(key, val);
-        } else {
-          ttstyle.removeProperty(key);
-        }
-      });
-    };
-
-    const hijackMouseTooltip = (evt) => {
-      const imgWidth = img.naturalWidth;
-      const imgHeight = img.naturalHeight;
-
-      if (player.controls() && imgWidth && imgHeight) {
-        let position = dom.getPointerPosition(seekBarEl, evt).x * player.duration();
-
-        position = position / options.interval;
-
-        const playerWidth = player.currentWidth();
-        const scaleFactor = responsive && playerWidth < responsive ?
-          playerWidth / responsive : 1;
-        const columns = imgWidth / width;
-        const scaledWidth = width * scaleFactor;
-        const scaledHeight = height * scaleFactor;
-        const cleft = Math.floor(position % columns) * -scaledWidth;
-        const ctop = Math.floor(position / columns) * -scaledHeight;
-        const bgSize = (imgWidth * scaleFactor) + 'px ' +
-                       (imgHeight * scaleFactor) + 'px';
-        const controlsTop = dom.findPosition(controls.el()).top;
-        const seekBarTop = dom.findPosition(seekBarEl).top;
-        // top of seekBar is 0 position
-        const topOffset = -scaledHeight - Math.max(0, seekBarTop - controlsTop);
-
-        tooltipStyle({
-          'width': scaledWidth + 'px',
-          'height': scaledHeight + 'px',
-          'background-image': 'url(' + url + ')',
-          'background-repeat': 'no-repeat',
-          'background-position': cleft + 'px ' + ctop + 'px',
-          'background-size': bgSize,
-          'top': topOffset + 'px',
-          'color': '#fff',
-          'text-shadow': '1px 1px #000',
-          'border': '1px solid #000',
-          'margin': '0 1px'
-        });
+      if (val !== '') {
+        ttstyle.setProperty(key, val);
+      } else {
+        ttstyle.removeProperty(key);
       }
-    };
+    });
+  };
 
+  const resetMouseTooltip = () => {
     tooltipStyle({
       'width': '',
       'height': '',
@@ -93,9 +51,77 @@ export default function spriteThumbs(player, options) {
       'border': '',
       'margin': ''
     });
+  };
 
+  const spriteready = () => {
+    const ready = url.length && height && width && mouseTimeDisplay;
+
+    resetMouseTooltip();
+    if (!ready) {
+      ['url', 'width', 'height'].forEach((key) => {
+        if (!options[key] || key === 'url' && !options[key].length) {
+          log('no spriteThumbnails ' + key + ' given');
+        }
+      });
+    }
+    return ready;
+  };
+
+  const hijackMouseTooltip = (evt) => {
+    const imgWidth = sprite.naturalWidth;
+    const imgHeight = sprite.naturalHeight;
+    const seekBarEl = seekBar.el();
+
+    if (sprite.complete && imgWidth && imgHeight) {
+      let position = dom.getPointerPosition(seekBarEl, evt).x * player.duration();
+
+      position = position / options.interval;
+
+      const responsive = options.responsive;
+      const playerWidth = player.currentWidth();
+      const scaleFactor = responsive && playerWidth < responsive ?
+        playerWidth / responsive : 1;
+      const columns = imgWidth / width;
+      const scaledWidth = width * scaleFactor;
+      const scaledHeight = height * scaleFactor;
+      const cleft = Math.floor(position % columns) * -scaledWidth;
+      const ctop = Math.floor(position / columns) * -scaledHeight;
+      const bgSize = (imgWidth * scaleFactor) + 'px ' +
+                     (imgHeight * scaleFactor) + 'px';
+      const controlsTop = dom.findPosition(controls.el()).top;
+      const seekBarTop = dom.findPosition(seekBarEl).top;
+      // top of seekBar is 0 position
+      const topOffset = -scaledHeight - Math.max(0, seekBarTop - controlsTop);
+
+      tooltipStyle({
+        'width': scaledWidth + 'px',
+        'height': scaledHeight + 'px',
+        'background-image': 'url(' + url + ')',
+        'background-repeat': 'no-repeat',
+        'background-position': cleft + 'px ' + ctop + 'px',
+        'background-size': bgSize,
+        'top': topOffset + 'px',
+        'color': '#fff',
+        'text-shadow': '1px 1px #000',
+        'border': '1px solid #000',
+        'margin': '0 1px'
+      });
+    } else {
+      resetMouseTooltip();
+    }
+  };
+
+  player.on('loadstart', () => {
+    log.level(videojs.log.level());
+  });
+
+  // preload sprite if completely configured at plugin level
+  if (spriteready()) {
+    sprite.src = url;
     progress.on('mousemove', hijackMouseTooltip);
     progress.on('touchmove', hijackMouseTooltip);
-    player.addClass('vjs-sprite-thumbnails');
   }
-}
+  player.addClass('vjs-sprite-thumbnails');
+};
+
+export default spriteThumbs;
